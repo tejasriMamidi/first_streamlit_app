3ff8b83ce17dd4b7a09a4b7bb95e63620e9c9b91
tejasriMamidi <70939822+tejasriMamidi@users.noreply.github.com>
2022-10-07 12:46:29 +0530

    Update streamlit_app.py
    Adding the indexs to fruits name

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text(' 🥚Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭Build Your own Fruit Smoothie ')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 # display the table on the page
