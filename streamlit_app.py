fc5f9499e73f0189ead526d8c5886aac32dcd25e
tejasriMamidi <70939822+tejasriMamidi@users.noreply.github.com>
2022-10-07 12:24:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title(" My Parents New Healthy divers")
-streamlit.header(':)Breakfast Menu')
+streamlit.header(':_nature: Breakfast Menu')
 streamlit.text(' -> Orange 3 & Bluberry Oatmeal')
 streamlit.text(' -->> kale ,Spinach & Rockett Smoothie')
 streamlit.text(' -- >Hard-Boiled Free-Range Egg')
