f2f35951317ea8e2d222194a29cc79f637f999de
tejasriMamidi <70939822+tejasriMamidi@users.noreply.github.com>
2022-10-07 12:53:20 +0530

    Update streamlit_app.py
    added other pandas attribut loc to fectch the required col

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ streamlit.header('🍌🥭Build Your own Fruit Smoothie ')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado'])
+fruit_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
