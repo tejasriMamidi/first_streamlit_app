22a6ae29e788aa8cb8c4c798b920c22eac94c4c9
tejasriMamidi <70939822+tejasriMamidi@users.noreply.github.com>
2022-10-07 12:43:14 +0530

    Update streamlit_app.py
    Add the multiselect option to pick the interest fruit in the below data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,8 @@ streamlit.text(' 🥚Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭Build Your own Fruit Smoothie ')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+# display the table on the page
+
 streamlit.dataframe(my_fruit_list)
