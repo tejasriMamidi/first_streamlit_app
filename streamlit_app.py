49ff3887bd5f89039c204572cacb433ebd59b822
tejasriMamidi <70939822+tejasriMamidi@users.noreply.github.com>
2022-10-07 12:48:34 +0530

    Update streamlit_app.py
    added default fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.header('🍌🥭Build Your own Fruit Smoothie ')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado ','Strawberries'])
 # display the table on the page
 
 streamlit.dataframe(my_fruit_list)
