a8ae7ee605970e60aef3ff78ad65e9dfaa93b698
tejasriMamidi <70939822+tejasriMamidi@users.noreply.github.com>
2022-10-07 12:39:33 +0530

    Update streamlit_app.py
    imported pandas package
    --> used read_csv function to read and pull the data into data frame
    -->used the streamlit library to display it on the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title(" My Parents New Healthy divers")
 streamlit.header(' 🥣 Breakfast Menu')
 streamlit.text('🍊 Orange 3 & Bluberry Oatmeal')
@@ -6,4 +7,5 @@ streamlit.text('🥬  kale ,Spinach & Rockett Smoothie')
 streamlit.text(' 🥚Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭Build Your own Fruit Smoothie ')
-
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
